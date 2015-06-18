40d36a060b26a56d29c800934621a91189332b24
Darrick Wiebe <dw@xnlogic.com>
2015-06-18 18:19:46 -0400

    fix route to /sessions endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
-  } do
+  }
+  resources :users do |u|
+    get 'welcome', on: :member
+  end
+
+  devise_scope :user do
     get '/login' => 'users/sessions#new', as: 'login'
     post '/sessions', to: 'users/sessions#create', as: 'sessions'
     delete '/sessions/logout', to: 'users/sessions#destroy', as: 'logout'
-  end
-  resources :users do |u|
-    get 'welcome', on: :member
   end
 end
