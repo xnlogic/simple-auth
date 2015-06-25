2d35db8bef7232799cf33dbbbde32be90d8e45e2
Darrick Wiebe <dw@xnlogic.com>
2015-06-24 23:19:52 -0400

    Fix invalid sign in

--- apps/simple-auth/app/controllers/users/sessions_controller.rb
+++ apps/simple-auth/app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  include Concerns::SignIn
+  attr_accessor :token, :token_error
 
   respond_to :json
 
@@ -7,13 +7,42 @@ class Users::SessionsController < Devise::SessionsController
   # def new
   #   super
   # end
+  #
 
   def create
-    self.resource = user = warden.authenticate!(auth_options)
-    # using SignIn concern...
-    respond_with_token do |format|
-      set_flash_message(:notice, :signed_in) if is_flashing_format?
-      respond_with resource, location: after_sign_in_path_for(resource)
+    user, opts = warden.send :_perform_authentication, auth_options
+    if user
+      self.resource = user
+      respond_to do |format|
+        format.json do
+          create_token(resource)
+          # Perform additional authentication based on status.
+          if token
+            render json: token
+          elsif token_error
+            render json: { error: token_error.message }, status: token_error.status
+          else
+            render json: {}, status: :unauthorized
+          end
+        end
+        format.html do
+          sign_in resource_name, resource
+          set_flash_message(:notice, :signed_in) if is_flashing_format?
+          respond_with resource, location: after_sign_in_path_for(resource)
+        end
+      end
+    else
+      respond_to do |format|
+        format.json do
+          render json: {}, status: :unauthorized
+        end
+        format.html do
+          flash[:error] = "Invalid email address or password"
+          param = params['user'] || {}
+          self.resource = User.new email: param['email']
+          render :new
+        end
+      end
     end
   end
 
@@ -33,5 +62,23 @@ class Users::SessionsController < Devise::SessionsController
 
   def after_sign_in_path_for(user)
     welcome_users_path
+  end
+
+  def create_token(user)
+    begin
+      self.token = user.create_api_token
+    rescue XN::Error::ApiError => e
+      self.token_error = {
+        status: (e.status || 400),
+        message: "Unable to generate token: #{ e.message }"
+      }
+    end
+  end
+
+  # Only call this when creating a user management session.
+  def sign_in(resource_name, user, *args)
+    create_token(user)
+    session['api_token'] = token ? token['token'] : nil
+    super
   end
 end
